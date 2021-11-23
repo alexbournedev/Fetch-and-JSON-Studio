47b230dc56e3393b100cc2349662bb29d1b0f651
Alex Bourne <afb989@outlook.com>
2021-11-22 21:04:09 -0500

    ride, sally ride

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+    return response.json();
+  }).then(function(json){
+    const result = json.sort((a,b) => (a.hoursInSpace < b.hoursInSpace) ? 1 : -1)
+			let wanda = "";
+    	let container = document.querySelector("#container");
+		
+			for(cosmo of json){
+				wanda += `
+				<div class="astronaut">
+   				<div class="bio">
+      			<h3>${cosmo.firstName} ${cosmo.lastName}</h3>
+      		<ul>
+         		<li>Hours in space: ${cosmo.hoursInSpace}</li>
+         		<li>Active: ${cosmo.active}</li>
+         		<li>Skills: ${cosmo.skills.join(", ")}</li>
+      		</ul>
+   				</div>
+   				<img class="avatar" src="${cosmo.picture}"></img>
+				</div>
+				`;
+			}
+			container.innerHTML = wanda;
+    })
+})
